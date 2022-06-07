0ace5eb874b2e8cdc527a56af16b535d27142215
Jakub Podolski <jakub.podolski@fortedigital.no>
2022-06-07 22:33:33 +0200

    Add data display

--- pages/starships/index.tsx
+++ pages/starships/index.tsx
@@ -1,13 +1,21 @@
 import React, { FC } from 'react'
 
+import styles from '../../styles/Starship.module.css'
 
-const Starships: FC<{}> = () => {
+interface StarshipsProps {
+  starships: {
+    name: string;
+    model: string;
+    id: string;
+  }[];
+}
+
+const Starships: FC<StarshipsProps> = (props) => {
+  const { starships } = props;
 
   return (
-    <div>
-      Starship
+    <div className={styles.grid}>
+      {starships.map((starship, id) => <StarshipCard key={id} {...starship}/>)}
     </div>
   )
 };
-
-export default Starships;
