e0ef6dc9881b332f2d56737065e0d9abadff2047
Jakub Podolski <jakub.podolski@fortedigital.no>
2022-06-07 22:19:35 +0200

    Adjust swapi client

--- swapiClient.ts
+++ swapiClient.ts
@@ -4,10 +4,35 @@ const rootApiPath = "https://swapi.dev/api";
 export class SwapiClient {
 
   public static getStarships = (): Promise<any> => {
-    return fetch(`${rootApiPath}/starships`).then(res => res.json())
+    const data = fetch(`${rootApiPath}/starships`)
+      .then(res => res.json())
+      .then(res => {
+
+        return res.results.map((ship: any) => ({
+          name: ship.name,
+          model: ship.model,
+          id: ship.url.split('/').reverse()[1]
+        }))
+      });
+
+      return data;
   }
 
   public static getStarshipById = (id: number): Promise<any> => {
-    return fetch(`${rootApiPath}/starships/${id}`).then(res => res.json())
+    const data = fetch(`${rootApiPath}/starships/${id}`)
+      .then(res => res.json())
+      .then(res => {
+        return {
+          name: res.name, 
+          model: res.model, 
+          manufacturer: res.manufacturer,
+          cost: res.cost_in_credits, 
+          length: res.length, 
+          maxSpeed: res.max_atmosphering_speed, 
+          cargoCapacity: res.cargo_capacity, 
+        }
+      });
+
+      return data;
   }
 }
